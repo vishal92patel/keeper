d34cf9605a47e93f3cc620f966264645c6f6b470
Patel <vishal.c.patel@capgemini.com>
2019-04-18 10:44:36 +0530

    jquery added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title = 'keeper';
+    constructor() {
+        $(document).ready(() => {
+            console.log($('body'));
+            console.log($('div:last-child').remove());
+            const scripts = $('body').prevObject[0].scripts;
+            console.log(scripts[scripts.length - 1].remove());
+        });
+    }
 }
